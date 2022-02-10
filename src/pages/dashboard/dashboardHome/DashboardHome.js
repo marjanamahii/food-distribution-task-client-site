9c5ec04b55055184bebbdcb4219eb4b7795ec3b5
marjanamahii <marjanamai777@gmail.com>
2022-02-10 21:04:04 +0600

    6th commit

--- src/pages/dashboard/dashboardHome/DashboardHome.js
+++ src/pages/dashboard/dashboardHome/DashboardHome.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashboardHome = () => {
     return (
         <div>
-            <h1 style={{ textAlign: 'center' }}>This is dashboard main page.</h1>
+            <h1 style={{ textAlign: 'center' }}>Welcome to Yooda Hostel Dashboard</h1>
         </div>
     );
 };
